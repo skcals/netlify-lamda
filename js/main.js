24caa8538b66775080402985902d90a0aefb382d
web zone <webzone76@gmail.com>
2018-12-31 23:03:18 +0530

    First commit

---
+++ js/main.js
@@ -0,0 +1,28 @@
+// const fetchData = async () => {
+//   await (await fetch("http://localhost:9000/getusers")).json();
+// };
+
+// fetchData()
+//   .then(res => {
+//     console.log(res);
+//   })
+//   .catch(err => console.log("err"));
+
+fetch("/.netlify/functions/getusers")
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(myJson) {
+    // console.log(myJson.hits);
+
+    const data = myJson.hits;
+
+    data.forEach(res => {
+      const box = document.getElementById("box");
+      const li = document.createElement("li");
+      li.className = "list-group-item";
+      const node = document.createTextNode(res.views);
+      li.appendChild(node);
+      box.appendChild(li);
+    });
+  });
